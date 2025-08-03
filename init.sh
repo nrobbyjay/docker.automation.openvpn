d0b0b8381f7a1b41e526b8bec7cdb0a9f703158b
Robby Jay Navarro <>
2025-08-04 04:41:56 +0800

    Correct syntaxes errors

--- init.sh
+++ init.sh
@@ -4,12 +4,13 @@ docker --version >/dev/null 2>&1 || echo "error: git is not installed"
 
 if ! docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -wq buildtovpn; then
  git clone https://github.com/kylemanna/docker-openvpn.git
- sudo docker buildx build -f docker-ovpn/Dockerfile -t buildtovpn .
+ cd docker-openvpn
+ sudo docker buildx build -t buildtovpn .
 fi
 
 if ! docker volume ls --format '{{.Name}}' | grep -wq 'vol_ovpn'; then
  sudo docker volume create vol_ovpn
- sudo docker run --rm -v vol_ovpn buildtovpn ovpn_genconfig -u udp://(curl ifconfig.me)
+ sudo docker run --rm -v vol_ovpn buildtovpn ovpn_genconfig -u udp://$(curl ifconfig.me)
  sudo docker run --rm -v vol_ovpn buildtovpn ovpn_genconfig ovpn_initpki
 fi
 
