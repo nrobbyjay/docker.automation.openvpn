667777b70668fe7d6a9a124efd87b005fb095661
Robby Jay Navarro <nrobbyjay@outlook.com>
2025-08-06 03:30:52 +0800

    Added keycreation.sh (not tested yet)

--- init.sh
+++ init.sh
@@ -15,4 +15,15 @@ if ! docker volume ls --format '{{.Name}}' | grep -wq 'vol_ovpn'; then
  sudo docker run -it --rm -v vol_ovpn:/etc/openvpn buildtovpn ovpn_initpki
 fi
 
-sudo docker compose -d up 
+sudo docker compose -d up
+
+#keycreation.sh will be created and the build repo will be deleted
+cd ~
+sudo rm -rf docker.automation.openvpn
+
+#keycreation.sh
+echo "echo You are about to create a client to access this server, please provide name for client:
+read CLIENTNAME
+sudo docker run -v vol_ovpn:/etc/openvpn --rm -it buildtovpn easyrsa build-client-full $CLIENTNAME nopass
+sudo docker run -v vol_ovpn:/etc/openvpn --rm -it buildtovpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn" > keycreation.sh
+sudo chmod +x keycreation.sh
