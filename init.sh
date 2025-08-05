4b8b5aa99b21c7b61d58f2e827d053145c69f74d
Robby Jay Navarro <nrobbyjay@outlook.com>
2025-08-06 04:02:57 +0800

    Integrate docker-compose.yaml file inside the sh script

--- init.sh
+++ init.sh
@@ -15,15 +15,28 @@ if ! docker volume ls --format '{{.Name}}' | grep -wq 'vol_ovpn'; then
  sudo docker run -it --rm -v vol_ovpn:/etc/openvpn buildtovpn ovpn_initpki
 fi
 
-sudo docker compose up -d
-
-#keycreation.sh will be created and the build repo will be deleted
 cd ~
 sudo rm -rf docker.automation.openvpn
 
-#keycreation.sh
+echo "services:
+ ovpn:
+  image: buildtovpn
+  ports:
+   - 1194:1194/udp
+  volumes:
+   - vol_ovpn:/etc/openvpn
+  cap_add:
+   - NET_ADMIN
+  restart: unless-stopped
+
+volumes:
+ vol_ovpn:
+  external: true" > docker-compose.yaml
+
 echo "echo You are about to create a client to access this server, please provide name for client:
 read CLIENTNAME
 sudo docker run -v vol_ovpn:/etc/openvpn --rm -it buildtovpn easyrsa build-client-full $CLIENTNAME nopass
 sudo docker run -v vol_ovpn:/etc/openvpn --rm -it buildtovpn ovpn_getclient $CLIENTNAME > $CLIENTNAME.ovpn" > keycreation.sh
 sudo chmod +x keycreation.sh
+
+sudo docker compose up -d
