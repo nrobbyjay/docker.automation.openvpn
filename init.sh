a5c7939ffb1498166f0ae86ddd2149d50ea8b941
Robby Jay Navarro <nrobbyjay@outlook.com>
2025-08-06 13:46:40 +0800

    Added function for client to use own gateway rather than tunnel's

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt upgrade -y
 git --version >/dev/null 2>&1 || echo "error: git is not installed"
 docker --version >/dev/null 2>&1 || echo "error: git is not installed"
+ipcalc --version >/dev/null 2>&1 || echo "error: ipcalc is not installed"
 
 if ! docker image ls --format '{{.Repository}}:{{.Tag}}' | grep -wq buildtovpn; then
  git clone https://github.com/kylemanna/docker-openvpn.git
@@ -15,6 +16,16 @@ if ! docker volume ls --format '{{.Name}}' | grep -wq 'vol_ovpn'; then
  PUBLIC_IP=$(curl -s ifconfig.me)
  sudo docker run --rm -v vol_ovpn:/etc/openvpn buildtovpn ovpn_genconfig -u udp://$PUBLIC_IP
  sudo docker run -it --rm -v vol_ovpn:/etc/openvpn buildtovpn ovpn_initpki
+ #create method to export docker bridge + redirect net traffic to client network instead of vpn tunnel
+ DOCKERNETWORKBRIDGE=$(sudo docker network inspect bridge --format '{{(index .IPAM.Config 0).Subnet}}')
+ NETWORK=$(ipcalc $DOCKERNETWORKBRIDGE | awk -F: '/Address/ {print $2}' | awk '{print $1}')
+ SUBNET=$(ipcalc $DOCKERNETWORKBRIDGE | awk -F: '/Netmask/ {print $2}' | awk '{print $1}')
+
+CMD='sed -i "/push \"block-outside-dns\"/d" /etc/openvpn/openvpn.conf'
+sudo docker run --rm -v vol_ovpn:/etc/openvpn buildtovpn sh -c "$CMD"
+
+CMD='echo "push \"route ${NETWORK} ${SUBNET}\"" >> /etc/openvpn/openvpn.conf'
+sudo docker run --rm -v vol_ovpn:/etc/openvpn buildtovpn sh -c "$CMD"
 fi
 
 sudo chmod +x newclient.sh
--- newclient.sh
+++ newclient.sh
@@ -2,3 +2,5 @@ echo You are about to create a client to access this server, please provide name
 read CLIENTNAME
 sudo docker run -v vol_ovpn:/etc/openvpn --rm -it buildtovpn easyrsa build-client-full $CLIENTNAME nopass
 sudo docker run -v vol_ovpn:/etc/openvpn --rm -it buildtovpn ovpn_getclient $CLIENTNAME > ~/$CLIENTNAME.ovpn
+~/$CLIENTNAME
+sed -i "/redirect-gateway def1/d" "$HOME/$CLIENTNAME"
