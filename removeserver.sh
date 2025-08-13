e6af93b732d7fc4c8eac19018568652fee45a7eb
Robby Jay Navarro <nrobbyjay@outlook.com>
2025-08-13 12:03:13 +0800

    fix syntax errors on compose files

--- removeserver.sh
+++ removeserver.sh
@@ -3,6 +3,6 @@ sudo docker compose -f portainer.yaml down
 
 sudo docker image rm buildtovpn
 sudo docker volume rm vol_ovpn
-sudo docker network rm ovpnnet
+sudo docker network rm ovpn_net
 
 sudo docker volume rm portainer_data
